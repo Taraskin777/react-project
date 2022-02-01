070e0ed81e411c9ae85c1b5717beb6c5512b15d5
Taras <taraskin2012@gmail.com>
2022-02-01 23:01:18 +0200

    add Header and Text

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const el = <h1>Hello React.js</h1>
+const App = () => {
+    return (
+        <>
+            <h1>Hello App component</h1>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Delectus nobis tempore doloremque ad eos nulla voluptates natus
+                molestias, laboriosam maxime, reprehenderit, dolorum doloribus
+                explicabo. Harum quaerat vitae sit laudantium ratione.
+            </p>
+        </>
+    )
+}
 
-ReactDOM.render(el, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
