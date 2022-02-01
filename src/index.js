a891764a90d00ce0539d8d7aa5cd501d81eaeeec
Taras <taraskin2012@gmail.com>
2022-02-01 23:10:31 +0200

    add props

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = (props) => {
+    return <h1>Hello {props.title}</h1>
+}
+
+const Text = () => {
+    return (
+        <>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Obcaecati ullam id laudantium neque, pariatur ipsum mollitia
+                incidunt excepturi, recusandae rem maiores nostrum unde
+                doloremque beatae eos repellat labore facilis. Tenetur.
+            </p>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Obcaecati ullam id laudantium neque, pariatur ipsum mollitia
+                incidunt excepturi, recusandae rem maiores nostrum unde
+                doloremque beatae eos repellat labore facilis. Tenetur.
+            </p>
+        </>
+    )
+}
+
 const App = () => {
     return (
         <>
-            <h1>Hello App component</h1>
-            <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Delectus nobis tempore doloremque ad eos nulla voluptates natus
-                molestias, laboriosam maxime, reprehenderit, dolorum doloribus
-                explicabo. Harum quaerat vitae sit laudantium ratione.
-            </p>
+            <Header title="React" />
+            <Header title="App.js" />
+            <Text />
         </>
     )
 }
