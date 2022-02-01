cba3004f8fd604ba8720fd689174bbcf34e8c958
Taras <taraskin2012@gmail.com>
2022-02-01 23:25:30 +0200

    add jsconfig

--- src/index.js
+++ src/index.js
@@ -1,37 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './components/App'
 
-const Header = (props) => {
-    return <h1>Hello {props.title}</h1>
-}
-
-const Text = () => {
-    return (
-        <>
-            <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Obcaecati ullam id laudantium neque, pariatur ipsum mollitia
-                incidunt excepturi, recusandae rem maiores nostrum unde
-                doloremque beatae eos repellat labore facilis. Tenetur.
-            </p>
-            <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Obcaecati ullam id laudantium neque, pariatur ipsum mollitia
-                incidunt excepturi, recusandae rem maiores nostrum unde
-                doloremque beatae eos repellat labore facilis. Tenetur.
-            </p>
-        </>
-    )
-}
-
-const App = () => {
-    return (
-        <>
-            <Header title="React" />
-            <Header title="App.js" />
-            <Text />
-        </>
-    )
-}
 
 ReactDOM.render(<App />, document.getElementById('root'))
