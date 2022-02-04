ad3fd81a079199251693b748f07eb191ec9bdd1c
Taras <taraskin2012@gmail.com>
2022-02-04 14:59:33 +0200

    add header, footer and main component

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Header } from 'components/Header/Header'
+import { Main } from 'components/Main/Main'
+import { Footer } from 'components/Footer/Footer'
+
+
+
+
+export const App = () => {
+    return (
+        <>
+            <Header />
+            <Main />
+            <Footer />
+            <h1>Hello World!</h1>
+        </>
+    )
+}
