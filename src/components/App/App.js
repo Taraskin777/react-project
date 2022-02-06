fea8329b6a39cd5f7c4476ecad6fe566ee4ec134
Taras <taraskin2012@gmail.com>
2022-02-06 17:59:16 +0200

    add adaptive.css and header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import { Main } from 'components/Main/Main'
 import { Footer } from 'components/Footer/Footer'
 import 'common/reset.css'
 import 'common/style.css'
+import 'common/adaptive.css'
 
 export const App = () => {
     return (
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,9 +2,39 @@ import React from 'react'
 
 export const Header = () => {
     return (
-        <div className='container'>
-          <div> Header
-          </div>
-        </div>
+        <>
+            <div className="section top-header">
+                <div className="container">
+                    <div>
+                        <div className="row header">
+                            <div className="logo">
+                                <a href="#"></a>
+                            </div>
+                            <nav className="navigation col-md-6">
+                                <div >
+                                    <ul className="menu">
+                                        <li className='first'>
+                                            <a href="#">HOME</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">INFO</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">BLOG</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">PORFOLIO</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">CONTACT</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
