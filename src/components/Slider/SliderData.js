3ade432f73b441137219095cce37a83604b0cdc9
Taras Noga <taraskin2012@gmail.com>
2022-04-05 14:32:39 +0300

    add slider

---
+++ src/components/Slider/SliderData.js
@@ -0,0 +1,11 @@
+export const SliderData = [
+    {
+        image: '/images/After-20.jpg',
+    },
+    {
+        image: '/images/History-13.jpg',
+    },
+    {
+        image: '/images/Karina-2.jpg',
+    },
+]
--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -3,14 +3,17 @@ import { Greeting } from 'components/Greeting/Greeting'
 import { BlogList } from 'components/main-blog/BlogList'
 import { IHave } from 'components/I-have/I-have'
 import { Newsletter } from 'components/Newsletter/Newsletter'
+import { ImageSlider } from 'components/Slider/ImageSlider'
+import { SliderData } from 'components/Slider/SliderData'
 
 export const Main = () => {
     return (
         <>
             <Greeting />
             <BlogList />
+            <ImageSlider slides={SliderData} />
             <IHave />
-            <Newsletter/>
+            <Newsletter />
         </>
     )
 }
